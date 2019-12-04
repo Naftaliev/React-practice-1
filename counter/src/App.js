479af26a68b831fcc81f1fff00bd5a06d933286a
Naftaliev <n2i1a95@yandex.ru>
2019-12-04 07:08:35 +0300

    complete

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,25 +1,48 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
+import './App.sass';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+      super();
+      this.state = {
+        count: 0
+      }
+      this.onStateMinus = this.onStateMinus.bind(this)
+    }
+
+    onStatePlus() {
+      this.setState(({count}) => {
+        return {
+          count : count + 1
+        }
+      })
+    }
+
+    onStateMinus() {
+      this.setState(({count}) => {
+        return {
+          count : count - 1
+        }
+      })
+    }
+ 
+  render() {    
+    const {count} = this.state;
+
+    return (
+        <div className="App">
+          <div className="uk-card uk-card-default uk-card-body">
+          <h3 className="uk-card-title">Counter</h3>
+          <div>{count}</div>
+          <button 
+                  onClick={() => this.onStatePlus()} 
+                  className= "uk-button uk-button-primary uk-margin-small-top">+</button>
+          <button onClick={this.onStateMinus} className="uk-button uk-button-secondary uk-margin-small-top">-</button>
+          </div>
+        </div>
+      );
+
+  }
 }
 
 export default App;
